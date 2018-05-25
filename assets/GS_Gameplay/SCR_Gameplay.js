fbb6d8a87307e3fb84ecbe81f8e78e3929eaa097
Minh Hoang Tuan <minhhtml5@gmail.com>
2018-05-25 13:30:34 +0700

    - Some jumping and flipping

--- assets/GS_Gameplay/SCR_Gameplay.js
+++ assets/GS_Gameplay/SCR_Gameplay.js
@@ -1,3 +1,6 @@
+var RAD_TO_DEG = 57.29577951308231;
+var DEG_TO_RAD = 0.0174532925199433;
+
 cc.Class({
     extends: cc.Component,
 
@@ -6,10 +9,27 @@ cc.Class({
             default: null,
             type: cc.Sprite
         },
+		sprBackground1: {
+            default: null,
+            type: cc.Sprite
+        },
+		sprBackground2: {
+            default: null,
+            type: cc.Sprite
+        },
     },
 
     // use this for initialization
     onLoad: function () {
+		this.SCREEN_H = 1920;
+		this.SCREEN_W = cc.director.getOpenGLView().getViewPortRect().width * this.SCREEN_H / cc.director.getOpenGLView().getViewPortRect().height;
+		
+		this.CAMERA_SPEED = 30;
+		
+		this.cameraY = 0;
+		
+		
+		
 		var instance = this;
         cc.eventManager.addListener({
 			event: cc.EventListener.TOUCH_ALL_AT_ONCE,
@@ -29,6 +49,43 @@ cc.Class({
 
     // called every frame
     update: function (dt) {
-
+		if (this.sprUnicorn.getComponent("SCR_Unicorn").y > this.cameraY) {
+			var delta = this.sprUnicorn.getComponent("SCR_Unicorn").y - this.cameraY;
+			this.cameraY += delta * this.CAMERA_SPEED * dt;
+		}
+		
+		this.sprBackground1.node.y = -(this.cameraY % this.SCREEN_H);
+		this.sprBackground2.node.y = this.sprBackground1.node.y + this.SCREEN_H;
     },
+	
+	
+	
+	// My own helper
+	distanceBetweenTwoPoint: function (x1, y1, x2, y2) {
+		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	},
+	angleBetweenTwoPoint: function (x1, y1, x2, y2) {
+		var angle = 0;
+		if (y2 == y1) {
+			if (x2 > x1)
+				angle = 90;
+			else if (x2 < x1)
+				angle = 270;
+		}
+		else {
+			angle = Math.atan((x2 - x1) / (y2 - y1)) * RAD_TO_DEG;
+			if (y2 < y1) {
+				angle += 180;
+			}
+			if (angle < 0) angle += 360;
+		}
+
+		return angle;
+	},
+	sin: function (angle) {
+		return Math.sin(angle * DEG_TO_RAD);
+	},
+	cos: function (angle) {
+		return Math.cos(angle * DEG_TO_RAD);
+	},
 });
--- assets/GS_Gameplay/Unicorn/SCR_Unicorn.js
+++ assets/GS_Gameplay/Unicorn/SCR_Unicorn.js
@@ -1,32 +1,45 @@
-var GRAVITY = 3000;
-var DRAG = 300;
-var UNICORN_FART_POWER = 5000;
+var SIZE				=  200;
+var GRAVITY 			= 3000;
+var MOVEMENT_DRAG 		=  700;
+var ROTATION_DRAG 		=  100;
+var FART_MOVEMENT_POWER = 5000;
+var FART_ROTATION_POWER =  600;
+
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-		x: 0,
-		y: 0,
-		angle: 0,
-		speedX: 0,
-		speedY: 0,
+		scrGameplayNode: {
+            default: null,
+            type: cc.Node
+        },
     },
 
     onLoad () {
+		this.x			= 0;
+		this.y			= 0;
+		this.a			= 0;
+		this.speedX		= 0;
+		this.speedY		= 0;
+		this.speedA		= 0;
 		
+		this.scrGameplay = this.scrGameplayNode.getComponent("SCR_Gameplay");
 	},
 
     start () {
-
+		
     },
 	
 	fart () {
-		this.speedY += UNICORN_FART_POWER;
+		this.speedX += this.scrGameplay.sin(this.a) * FART_MOVEMENT_POWER;
+		this.speedY += this.scrGameplay.cos(this.a) * FART_MOVEMENT_POWER;
+		this.speedA = (0.5 + Math.random() * 0.5) * FART_ROTATION_POWER;
 	},
 
     update (dt) {
-		var drag = DRAG * dt;
+		// Apply movement drag
+		var drag = MOVEMENT_DRAG * dt;
 		
 		if (this.speedX > drag) this.speedX -= drag;
 		else if (this.speedX < -drag) this.speedX += drag;
@@ -36,12 +49,33 @@ cc.Class({
 		else if (this.speedY < -drag) this.speedY += drag;
 		else this.speedY = 0;
 		
-		
-		
+		// Apply gravity
 		this.speedY -= GRAVITY * dt;
-		this.y += this.speedY * dt;
 		
-		this.node.x = this.x;
-		this.node.y = this.y;
+		// Apply rotational drag
+		drag = ROTATION_DRAG * dt;
+		
+		if (this.speedA > drag) this.speedA -= drag;
+		else if (this.speedA < -drag) this.speedA += drag;
+		else this.speedA = 0;
+		
+		// Apply speed to actual properties
+		this.x += this.speedX * dt;
+		this.y += this.speedY * dt;
+		this.a += this.speedA * dt;
+		
+		if (this.speedX < 0 && this.x < -this.scrGameplay.SCREEN_W * 0.5) {
+			this.x = -this.scrGameplay.SCREEN_W * 0.5;
+			this.speedX = -this.speedX;
+		}
+		else if (this.speedX > 0 && this.x > this.scrGameplay.SCREEN_W * 0.5) {
+			this.x = this.scrGameplay.SCREEN_W * 0.5;
+			this.speedX = -this.speedX;
+		}
+		
+		// Apply properties to node's
+		this.node.x 		= this.x;
+		this.node.y			= this.y - this.scrGameplay.cameraY;
+		this.node.rotation 	= this.a;
 	},
 });
